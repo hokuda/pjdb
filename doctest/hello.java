50e2cc751e33aa525287ab52571a09df085515e8
Hisanobu Okuda <hisanobu.okuda@gmail.com>
2020-02-23 12:27:03 +0900

    fix _find_file

---
+++ doctest/hello.java
@@ -0,0 +1,7 @@
+public class hello {
+    public static void main(String[] args) {
+        String message = "hello";
+        message += "\n";
+        System.out.print(message);
+    }
+}
---
+++ doctest/org/test/hello.java
@@ -0,0 +1,9 @@
+package org.test;
+
+public class hello {
+    public static void main(String[] args) {
+        String message = "hello";
+        message += "\n";
+        System.out.print(message);
+    }
+}
